9d6011948196d253a0c732d3d461291be54e3341
Charles Oliver Nutter <headius@headius.com>
2009-07-26 20:48:08 -0500

    DSLify things a little bit and move jars to lib.

--- jibernate/eventlog.rb
+++ jibernate/eventlog.rb
@@ -2,13 +2,31 @@
 require 'rubygems'
 require 'java'
 require 'jdbc/hsqldb'
+require 'jruby/core_ext'
 
 # Our requires
-require 'event'
 require 'hibernate'
+require 'hibernate_config'
 
 # Point at config file
-Hibernate.config = "rails_underground.cfg.xml"
+Hibernate.dialect = Hibernate::Dialects::HSQL
+Hibernate.current_session_context_class = "thread"
+
+Hibernate.connection_driver_class = "org.hsqldb.jdbcDriver"
+Hibernate.connection_url = "jdbc:hsqldb:file:jibernate"
+Hibernate.connection_username = "sa"
+Hibernate.connection_password = ""
+Hibernate.properties["hbm2ddl.auto"] = "update"
+
+class Event
+  extend Hibernate::Model
+
+  hibernate_attr :id => :long, :title => :string, :date => :date
+
+  hibernate!
+end
+
+Hibernate.add_model "Event.hbm.xml"
 
 Hibernate.tx do |session|
   # Hack for HSQLDB's write delay
--- jibernate/hibernate.rb
+++ jibernate/hibernate.rb
@@ -1,17 +1,78 @@
 require 'java'
-$: << File.join(File.dirname(__FILE__),
-  "hibernate-distribution-3.3.2.GA")
-require 'lib/required/antlr-2.7.6.jar'
-require 'lib/required/commons-collections-3.1.jar'
-require 'lib/required/dom4j-1.6.1.jar'
-require 'lib/required/javassist-3.9.0.GA.jar'
-require 'lib/required/jta-1.1.jar'
-require 'slf4j-1.5.8/slf4j-simple-1.5.8.jar'
-require 'slf4j-1.5.8/slf4j-api-1.5.8.jar'
-require 'hibernate3.jar'
+require 'lib/antlr-2.7.6.jar'
+require 'lib/commons-collections-3.1.jar'
+require 'lib/dom4j-1.6.1.jar'
+require 'lib/javassist-3.9.0.GA.jar'
+require 'lib/jta-1.1.jar'
+require 'lib/slf4j-simple-1.5.8.jar'
+require 'lib/slf4j-api-1.5.8.jar'
+require 'lib/hibernate3.jar'
+require 'stringio'
+
+require 'dialects'
 
 module Hibernate
   import org.hibernate.cfg.Configuration
+  import javax.xml.parsers.DocumentBuilderFactory
+  import org.xml.sax.InputSource
+  JClass = java.lang.Class
+  JVoid = java.lang.Void::TYPE
+  DOCUMENT_BUILDER_FACTORY = DocumentBuilderFactory.new_instance
+  DOCUMENT_BUILDER_FACTORY.validating = false
+  DOCUMENT_BUILDER_FACTORY.expand_entity_references = false
+  DOCUMENT_BUILDER = DOCUMENT_BUILDER_FACTORY.new_document_builder
+
+  def self.dialect=(dialect)
+    config.set_property "hibernate.dialect", dialect
+  end
+
+  def self.current_session_context_class=(ctx_cls)
+    config.set_property "hibernate.current_session_context_class", ctx_cls
+  end
+
+  def self.connection_driver_class=(driver_class)
+    config.set_property "hibernate.connection.driver_class", driver_class
+  end
+
+  def self.connection_url=(url)
+    config.set_property "hibernate.connection.url", url
+  end
+
+  def self.connection_username=(username)
+    config.set_property "hibernate.connection.username", username
+  end
+
+  def self.connection_password=(password)
+    config.set_property "hibernate.connection.password", password
+  end
+  
+  class PropertyShim
+    def initialize(config)
+      @config = config
+    end
+    
+    def []=(key, value)
+      key = ensure_hibernate_key(key)
+      @config.set_property key, value
+    end
+
+    def [](key)
+      key = ensure_hibernate_key(key)
+      config.get_property key
+    end
+
+    private
+    def ensure_hibernate_key(key)
+      unless key =~ /^hibernate\./
+        key = 'hibernate.' + key
+      end
+      key
+    end
+  end
+
+  def self.properties
+    PropertyShim.new(@config)
+  end
 
   def self.tx
     session.begin_transaction
@@ -22,12 +83,6 @@ module Hibernate
   end
 
   def self.factory
-    config = Configuration.new
-    if @filename
-      config.configure @filename
-    else
-      config.configure
-    end
     @factory ||= config.build_session_factory
   end
 
@@ -35,7 +90,43 @@ module Hibernate
     factory.current_session
   end
 
-  def self.config=(filename)
-    @filename = filename
+  def self.config
+    @config ||= Configuration.new
+  end
+
+  def self.add_model(mapping)
+    config.add_input_stream(File.open(mapping).to_inputstream)
+  end
+
+  module Model
+    TYPES = {
+      :string => java.lang.String,
+      :long => java.lang.Long,
+      :date => java.util.Date
+    }
+
+    def hibernate_sigs
+      @hibernate_sigs ||= {}
+    end
+
+    def hibernate_attr(attrs)
+      attrs.each do |name, type|
+        attr_accessor name
+        get_name = "get#{name.to_s.capitalize}"
+        set_name = "set#{name.to_s.capitalize}"
+
+        alias_method get_name.intern, name
+        add_method_signature get_name, [TYPES[type].java_class]
+        alias_method set_name.intern, :"#{name.to_s}="
+        add_method_signature set_name, [JVoid, TYPES[type].java_class]
+      end
+    end
+    
+    def hibernate!
+      become_java!
+
+#      Hibernate.mappings.
+#      Hibernate.add_mapping reified_class,
+    end
   end
 end
